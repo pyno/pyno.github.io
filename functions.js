3c68e87936bd23964002c5bb82cc1320e1fd6979
pyno <pyno.email@gmail.com>
2016-09-11 19:29:20 +0200

    Added and managed the 'about' anchor for mobile devices

---
+++ functions.js
@@ -0,0 +1,19 @@
+function goto_about(){
+	window.location="index.html#about";
+
+	document.getElementById("about_li").className+=" active";
+	document.getElementById("home").className="";
+	document.getElementById("research").className="";
+	document.getElementById("projects").className="";
+}
+
+function load(){
+
+	if(window.location.href.indexOf("#about")> -1){
+		document.getElementById("about_li").className+=" active";
+		document.getElementById("home").className="";
+		document.getElementById("research").className="";
+		document.getElementById("projects").className="";
+	}
+}
+
